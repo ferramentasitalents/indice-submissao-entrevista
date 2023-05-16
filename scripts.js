a2b0dd6685440afc0edf1e2ad6b88a472d23d235
ferramentasitalents <133061937+ferramentasitalents@users.noreply.github.com>
2023-05-16 15:56:21 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function calculateIndex() {
   const numSubmetidos = parseFloat(document.getElementById("num-submetidos").value);
   const numEntrevistados = parseFloat(document.getElementById("num-entrevistados").value);
-  const index = (numSubmetidos / numEntrevistados);
+  const index = numSubmetidos / numEntrevistados;
   document.getElementById("result").innerHTML = `Índice de submissão para entrevista: ${index.toFixed(2)}%`;
 }
 
